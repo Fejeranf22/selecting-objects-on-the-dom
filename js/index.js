f7e8a1463fced28ffc5a146f9bff36c6383abcf4
ty fejerang <fejerang.ty@gmail.com>
2018-01-27 13:44:28 -0800

    added the querySelector method and documented how to use it

--- js/index.js
+++ js/index.js
@@ -6,3 +6,14 @@ console.log(list2);
 
 var list3 = document.getElementsByTagName("li");
 console.log(list3);
+
+var qSelect = document.querySelector("#idTag");
+console.log(qSelect);
+
+var qSelect1 = document.querySelector(".className");
+console.log(qSelect1);
+
+
+var qSelect2 = document.querySelectorAll(".className");
+console.log(qSelect2);
+console.log(qSelect2.length);
