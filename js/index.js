32924b9a953cbfd12024f87591e6832e3fa2dbf2
ty fejerang <fejerang.ty@gmail.com>
2018-01-27 13:32:26 -0800

    this is the initial commit of a project i coded along with from udemy: access objects on the dom from the javascript index.

---
+++ js/index.js
@@ -0,0 +1,8 @@
+var list1 = document.getElementById("idTag");
+console.log(list1);
+
+var list2 = document.getElementsByClassName("className");
+console.log(list2);
+
+var list3 = document.getElementsByTagName("li");
+console.log(list3);
